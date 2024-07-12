fee91ae5448001f99114c60e434235e281f2e85a
Baseer Rahimi <mohammadbaseer25@gmail.com>
2024-07-13 00:39:56 +0200

    Static Page Fixed the image Opt

--- src/app/(pages)/staticpage/page.tsx
+++ src/app/(pages)/staticpage/page.tsx
@@ -1,4 +1,5 @@
 import {  Character } from "@/app/models/custom-types";
+import Image from "next/image";
 
 const StaticPage = async () => {
 const randomId = Math.floor(Math.random() * 800)
@@ -12,7 +13,10 @@ const result:Character = await response.json();
         <h2>This page is already rendered into server nad served to the browser when we navigate to it. every time you reload the page (in deployed version) you will get the same context</h2>
         <hr />
         <h2>Name: {result.name}</h2>
-      <img src={result.image} alt={result.name} />
+      {/* <img src={result.image} alt={result.name} />
+       */}
+<Image src={result.image} alt={result.name} width={200} height={200} />
+
     </div>
   )
 }
