dc146d3132b151c66cf299722be9e437cdad795b
Baseer Rahimi <mohammadbaseer25@gmail.com>
2024-07-13 12:02:03 +0200

    Modified

--- src/app/(pages)/staticpage/page.tsx
+++ src/app/(pages)/staticpage/page.tsx
@@ -1,24 +1,23 @@
-import {  Character } from "@/app/models/custom-types";
+import { Character } from "@/app/models/custom-types";
 import Image from "next/image";
 
 const StaticPage = async () => {
-const randomId = Math.floor(Math.random() * 800)
-console.log("Random ", randomId)
+  const randomId = Math.floor(Math.random() * 800);
+  console.log("Random ", randomId);
 
-const response = await fetch(`https://rickandmortyapi.com/api/character/${randomId}`);
-const result:Character = await response.json();
+  const response = await fetch(`https://rickandmortyapi.com/api/character/${randomId}`);
+  const result: Character = await response.json();
 
   return (
     <div>
-        <h2>This page is already rendered into server nad served to the browser when we navigate to it. every time you reload the page (in deployed version) you will get the same context</h2>
-        <hr />
-        <h2>Name: {result.name}</h2>
+      <p>This page is already rendered into server nad served to the browser when we navigate to it. every time you reload the page (in deployed version) you will get the same context</p>
+      <hr />
+      <h2>Name: {result.name}</h2>
       {/* <img src={result.image} alt={result.name} />
        */}
-<Image src={result.image} alt={result.name} width={200} height={200} />
-
+      <Image src={result.image} alt={result.name} width={200} height={200} />
     </div>
-  )
-}
+  );
+};
 
-export default StaticPage
+export default StaticPage;
